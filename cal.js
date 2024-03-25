1fae253c27a3edf29c95bfbbf5f4d64ec0d4479f
KHAN MAHMUD ARIF RABBANI <164653926+Arif-Rabbani-188@users.noreply.github.com>
2024-03-25 15:15:27 +0600

    Update cal.js

--- cal.js
+++ cal.js
@@ -6,12 +6,15 @@ let clickedBtn = "";
 let clbtn ="";
 let count = 0;
 let num1= 0;
+let num2= 0;
+let opp=0;
+let opp1 =1;
 
 const reset = () =>{
     screen.innerHTML = "0";
 }
 
-let op = ["AC", "+/-", "%", "/", "*", "-", "+"];
+let op = ["AC", "+/-", "%", "/", "*", "-", "+", "="];
 
 allBtns.forEach(bt =>{
     bt.addEventListener("click", (e)=>{
@@ -21,10 +24,50 @@ allBtns.forEach(bt =>{
             num1 = clickedBtn*1;
             clickedBtn ="";
             screen.innerHTML = clickedBtn;
+          if (clbtn === "AC") {
+             screen.innerHTML = "";
+             num1 = 0;
+             num2 = 0;
+             opp = 0;
+             opp1 = 1;
+          }else if (clbtn === "+") {
+             screen.innerHTML = opp+num1;
+            opp = num1 + opp;
+          }else if (clbtn === "-"){
+            if (opp === 0) {
+              screen.innerHTML = num1-opp;
+              opp = num1 - opp;   
+            }else{
+              screen.innerHTML = opp-num1;
+              opp = opp - num1;
+            }   
+          }else if (clbtn === "*") {
+               screen.innerHTML = num1*opp1;
+            opp1 = num1 * opp1;
+          }else if (clbtn === "/") {
+             if (opp1 === 1) {
+                screen.innerHTML = num1/opp1;
+                opp1 = num1 / opp1;
+             }else{
+                screen.innerHTML = opp1/num1;
+                opp1 = opp1 / num1;
+             }
+             
+          }else if (clbtn === "%") {
+             screen.innerHTML = num1/100;
+             opp = num1 / 100;
+          }else if (clbtn === "+/-") {
+             screen.innerHTML = -num1;
+             opp = -num1;
+          }
+          
+          
+          
+      
         }else{
             clickedBtn += e.target.innerHTML;
             screen.innerHTML = clickedBtn;
-        }
-         console.log(num1);
+        };
+         console.log(num1,opp);
     });
 });
