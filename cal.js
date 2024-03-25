8089e6cbc5b1df332d3f28e675f29cefa94c71b5
KHAN MAHMUD ARIF RABBANI <164653926+Arif-Rabbani-188@users.noreply.github.com>
2024-03-25 06:26:40 +0600

    Update cal.js
    Operator fix

--- cal.js
+++ cal.js
@@ -1,22 +1,30 @@
 let allBtns = document.querySelectorAll(".box");
+
 let screen = document.querySelector(".screen");
+
 let clickedBtn = "";
 let clbtn ="";
 let count = 0;
 let num1= 0;
+
 const reset = () =>{
     screen.innerHTML = "0";
 }
+
 let op = ["AC", "+/-", "%", "/", "*", "-", "+"];
+
 allBtns.forEach(bt =>{
     bt.addEventListener("click", (e)=>{
         clbtn = e.target.innerHTML;
+
         if(op.includes(clbtn)){
-            num1 = clickedBtn;
+            num1 = clickedBtn*1;
             clickedBtn ="";
+            screen.innerHTML = clickedBtn;
+        }else{
+            clickedBtn += e.target.innerHTML;
+            screen.innerHTML = clickedBtn;
         }
-        clickedBtn += e.target.innerHTML;
-        screen.innerHTML = clickedBtn;
-        console.log(num1);
+         console.log(num1);
     });
 });
